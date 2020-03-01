b8ff65a012d1459384599cb0f64e844446fe0394
Sergey Gubar <sergey.gubar187@gmail.com>
2020-03-01 12:43:30 +0200

    Add RAM output

--- lab1/task1.sh
+++ lab1/task1.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # ./task1.sh -h | --help -n num file
 # Write all data to file, or to ~/bash/task1.out if it's not specified
-echo "Date: $(date)" > task1.out
-echo "--- Hardware ---" >> task1.out
-cat /proc/cpuinfo | grep 'model name' | uniq >> task1.out
+output='out.out'
+echo "Date: $(date)" > $output 
+echo "--- Hardware ---" >> $output 
+
+cat /proc/cpuinfo | grep 'model name' | uniq >> $output
+free -m | grep Mem: | awk '{print $2}' >> $output
+
