f5164256d3e38f5c24cc6abfccb9f28de96c8cd2
Gubar Sergey <sergey.gubar187@gmail.com>
2020-03-04 15:34:24 +0200

    Fix logic with filenames

--- lab1/task1.sh
+++ lab1/task1.sh
@@ -60,7 +60,6 @@ collectInfo() {
     echo "Kernel version: $kernel_version" >> $1
 
     created_info=$(dumpe2fs $(mount | grep 'on / ' | awk '{print $1}') | grep 'Filesystem created: ')
-    echo "AAAA"
     echo "$created_info"
     if [ -z $created_info ]; then
         >&2 echo "Failed to fetch installation date info, skipping..."
@@ -116,54 +115,58 @@ fi
 while getopts "n:" OPTION; do
     if [[ "$OPTARG" =~ ^-?[0-9]+$ ]]; then
         if ! (($OPTARG >= 1)); then
-            print_error "-n option must be an integer > 1, got $OPTARG"
+            print_error "-n must be an integer > 1, got $OPTARG"
         else
-            n=$OPTARG
+            _n=$OPTARG
         fi
     else
         print_error "-n must be an integer, got $OPTARG"
     fi
 done
-
 shift $(($OPTIND - 1))
-file=$1
+_file=$1
 
-if [ -z "$n" ]; then
-    n=-1
+# if arguments not specified
+if [ -z "$_n" ]; then
+    _n=-1
 fi
 
-if [ -z "$file" ]; then
-    file="$HOME/bash/task1.out"
+# if arguments not specified
+if [ -z "$_file" ]; then
+    _file="$HOME/bash/task1.out"
 fi
 
-parent_directory=$(dirname "$file")
-file_name=$(basename $file)
+parent_directory=$(dirname "$_file")
+file_name=$(basename $_file)
+# check that directory for output exists
 if [ ! -d $parent_directory ]; then
     echo "Directory $parent_directory does not exist, creating..."
     error_msg=$(mkdir -p "$parent_directory" 2>&1)
+    # check if creating a directory failed
     if [ $? -ne 0 ]; then
         print_error "Error creating directory $parent_directory, reason: $error_msg"
     fi
 fi
 
-if [ -f "$file" ]; then
+if [ -f "$_file" ]; then
     cur_d=$(date "+%Y%m%d")
 
-    file="${file}-${cur_d}"
+    _file="${_file}-${cur_d}"
     # we get all the files with -nnnn in their names, sorted numerically from lowest to highest
-    allfiles_created_before=($(ls -1v "$parent_directory" | grep -E "^$file_name-[0-9]{4,}"))
-    num_found=${#allfiles_created_before[@]}
-    if [ $num_found -eq 0 ]; then # if no such files found (i.e. len(allfiles...) == 0)
-        file="${file}-0000"
+    all_files_created_before=($(ls -1v "$parent_directory" | grep -E "^$file_name-[0-9]{4,}"))
+    num_found=${#all_files_created_before[@]}
+    if [ $num_found -eq 0 ]; then # if no such files found (i.e. len(all_files...) == 0)
+        _file="${_file}-0000"
     else
-        last_createdfile_name=${allfiles_created_before[$((num_found - 1))]}
+        last_createdfile_name=${all_files_created_before[$((num_found - 1))]}
+        # remove the largest possible '*-' string from the beginning of the variable's contents
         last_created_number=${last_createdfile_name##*-}
-        last_created_number=$(expr "$last_created_number" + 1)
+        last_created_number=$(expr "$last_created_number" + 1) # we use expr because otherwise 0100 -> 65 (octal -> dec)
         new_created_number="$(printf "%04d" $last_created_number)"
-        file="${file}-${new_created_number}"
-        if [ $n -ne -1 ] && [ $n -le $num_found ]; then
-            leave_nfiles=$(expr $num_found - $_n + 1) #
-            for var in ${allfiles_created_before[@]:0:$leave_nfiles}; do
+        _file="${_file}-${new_created_number}"
+        if [ $_n -ne -1 ] && [ $_n -le $num_found ]; then
+            leave_n_files=$(expr $num_found - $_n + 1) # we delete +1 file because we will create a new just after
+            for var in ${all_files_created_before[@]:0:$leave_n_files}; do
                 echo "Deleting old output file ${var}..."
                 rm "${parent_directory}/${var}"
             done
@@ -171,5 +174,5 @@ if [ -f "$file" ]; then
     fi
 fi
 
-collectInfo $file
-echo "Created output file $file at $(date)."
+collectInfo $_file
+echo "Created output file $_file at $(date)."
