b8f4e2b01790f1732b5cbe5b1bd2380781b6ce0b
Gubar Sergey <sergey.gubar187@gmail.com>
2020-04-09 11:05:07 +0300

    Refactoring

--- lab4/pzpi-16-3-hubar-serhii-lab4.sh
+++ lab4/pzpi-16-3-hubar-serhii-lab4.sh
@@ -4,38 +4,60 @@ if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
     echo "Usage ./pzpi-16-3-hubar-serhii-lab4.sh [-h | --help]";
     echo "Script launches child process and waits for interactive communication with user";
     echo "Possible interaction options:";
-    echo "1) test - send ping signal to child subshell";
+    echo "1) ping - send ping signal to child subshell";
     echo "2) kill - to kill child subshell"
     echo "3) back - to send ping back (child subshell will respond)"
     echo "q to quit"
     exit 0;
 fi
 
-if [ "$#" -ne 0 ]; then
+localize_error() {
     if [[ $LANG =~ uk_UA ]]; then
-        echo "Скрипт не може приймати аргументів крім --help" 1>&2
+        echo "$1" 1>&2
     else
-        echo "Script can't be run with arguments (excuding --help)" 1>&2
+        echo  "$2" 1>&2
     fi
+}
+
+if [ "$#" -ne 0 ]; then
+    localize_error "Скрипт не може приймати аргументів крім --help" "Script can't be run with arguments (excuding --help)"
     exit 1
 fi
 
-if [ ! -d "$HOME/log" ]; then
-    mkdir "$HOME/log"
-fi
 
-log_path="$HOME/log/pzpi-16-3-hubar-serhii-lab4.log"
+path_to_log_file="$HOME/log/pzpi-16-3-hubar-serhii-lab4.log"
 
-if [ ! -f "${log_path}" ]; then
-    touch "${log_path}"
-fi
+check_dir() {
+    if [ ! -d "$HOME/log" ]; then
+        mkdir "$HOME/log"
+    fi
+}
 
-log_message() {
-    current_date=$(date '+%a, %d %b %Y %X %z')
-    timestamp=$(date +%s)
-    message=$(printf "%s; %s; %s" "$current_date" "$timestamp" "$1")
-    logger "\"${message}\""
-    echo $message >> "${log_path}"
+check_log() {
+    if [ ! -f "${path_to_log_file}" ]; then
+        touch "${path_to_log_file}"
+    fi
+}
+
+check_dir
+
+check_log
+
+write_to_log() {
+    log_date=$(date '+%a, %d %b %Y %X %z')
+    unix_temistamp=$(date +%s)
+    formatted_message=$(printf "%s; %s; %s" "$log_date" "$unix_temistamp" "$1")
+    echo $formatted_message >> "${path_to_log_file}"
+    logger "\"${formatted_message}\""
+}
+
+start_log() {
+    write_to_log "${current_pid}; -1; INIT; Process start"
+    write_to_log "${child}; -1; INIT; Child Process start"
+}
+
+finish_log() {
+    write_to_log "${current_pid}; -1; END; Parent process stop"
 }
 
 send_test_signal_to_child() {
@@ -43,103 +65,100 @@ send_test_signal_to_child() {
     echo "Sent test signal to child process"
 }
 
-send_ping_back_signal_to_child() {
-    kill -SIGUSR2 "$child"
-    echo "Sent ping back signal to child process"
-}
-
 kill_child_process() {
     kill -SIGTERM "$child"
     echo "Killed child process"
 }
 
-parent_term() { 
+send_ping_back_signal_to_child() {
+    kill -SIGUSR2 "$child"
+    echo "Sent ping back signal to child process"
+}
+
+on_parent_terminated() { 
     kill -SIGTERM "$child"
-    log_message "${current_pid}; 15; SIGTERM; parent exits and throws SIGTERM to subshell"
+    write_to_log "${current_pid}; 15; SIGTERM; parent exits and throws SIGTERM to subshell"
     echo "Sent sigterm to child ${child}"
     exit 1
 }
 
-parent_interruption() {
-    log_message "${current_pid}; 2; SIGINT; parent got interrupted"
+on_parent_interrupted() {
+    write_to_log "${current_pid}; 2; SIGINT; parent got interrupted"
     exit 1
 }
 
-parent_ping() {
-    log_message "${current_pid}; 12; SIGUSR2; nothing, parent confirms that child process pinged back"
+on_parent_pinged() {
+    write_to_log "${current_pid}; 12; SIGUSR2; nothing, parent confirms that child process pinged back"
 }
 
 
+# Child process
 
 (
-child_term() {
+on_child_terminated() {
     pid=$(exec sh -c 'echo "$PPID"')
-    log_message "${pid}; 15; SIGTERM; child process terminated"
+    write_to_log "${pid}; 15; SIGTERM; child process terminated"
     exit 1
 }
-
-custom_signal() {
+on_child_ping_back() {
     pid=$(exec sh -c 'echo "$PPID"')
-    log_message "${pid}; 10; SIGUSR1; just ping signal"
-}
-
-ping_back_signal() {
-    pid=$(exec sh -c 'echo "$PPID"')
-    log_message "${pid}; 12; SIGUSR2; ping father back"
+    write_to_log "${pid}; 12; SIGUSR2; ping father back"
     kill -SIGUSR2 $$
 }
-
-heartbeat() {
+on_child_pinged() {
     pid=$(exec sh -c 'echo "$PPID"')
-    log_message "${pid}; 18; SIGCONTv; heartbeat"
+    write_to_log "${pid}; 10; SIGUSR1; just ping signal"
+}
+on_child_heartbeat() {
+    pid=$(exec sh -c 'echo "$PPID"')
+    write_to_log "${pid}; 18; SIGCONTv; on_child_heartbeat"
 }
 
-echo "Inside the subshell"
 
-trap child_term SIGTERM
-trap custom_signal SIGUSR1
-trap ping_back_signal SIGUSR2
-trap heartbeat 18
+trap on_child_terminated SIGTERM
+trap on_child_pinged SIGUSR1
+trap on_child_ping_back SIGUSR2
+trap on_child_heartbeat 18
+
+
+# Script
 
 while true; do
-    echo "Child is working..." 1>/dev/null
-    sleep 5
+    echo "Child process is doing some work..." 1>/dev/null
+    sleep 2
 done
 ) &
+
+# Script start
 
 
 child=$!
 current_pid=$$
 
-log_message "${current_pid}; -1; INIT; Process start"
-log_message "${child}; -1; INIT; Child Process start"
+start_log
 
-echo "Created child process ${child}"
-echo "My process ${current_pid}"
-
-trap parent_term SIGTERM
-trap parent_interruption SIGINT
-trap parent_ping SIGUSR2
+trap on_parent_pinged SIGUSR2
+trap on_parent_interrupted SIGINT
+trap on_parent_terminated SIGTERM
 
 (
     while true; do
         kill -18 "$child" 2>/dev/null
-        sleep 3
+        sleep 2
     done
 ) &
 
 ping_pid=$!
-echo "Ping pid ${ping_pid}"
 
-while read -r -p "What you wanna do? For options - open --help " && [[ $REPLY != q ]]; do
+while read -r -p "Select your option. For options - open --help " && [[ $REPLY != q ]]; do
   case $REPLY in
-    test) send_test_signal_to_child;;
+    ping) send_test_signal_to_child;;
     kill) kill_child_process;;
     back) send_ping_back_signal_to_child;;
-    *) echo "Try Again.";;
+    *) echo "Please, Try Again.";;
   esac
 done
 
-log_message "${current_pid}; -1; END; Parent process stop"
+finish_log
 
 kill $ping_pid
